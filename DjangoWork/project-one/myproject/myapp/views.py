b1a53f6753cb56ea800cfbb0e28721b897e2f80c
Farhan_waseer <59307895+farhanwaseer@users.noreply.github.com>
2024-02-09 23:25:38 +0000

    sasa

--- DjangoWork/project-one/myproject/myapp/views.py
+++ DjangoWork/project-one/myproject/myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
 
 def home(request):
     return HttpResponse("<h1>Hello world! Django app created")
