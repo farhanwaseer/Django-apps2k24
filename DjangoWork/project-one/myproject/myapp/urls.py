62bb4c472e20ce1d1e4775c29b05831d548813f8
Farhan_waseer <59307895+farhanwaseer@users.noreply.github.com>
2024-02-10 00:30:44 +0000

    sss

--- DjangoWork/project-one/myproject/myapp/urls.py
+++ DjangoWork/project-one/myproject/myapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('farhan/', views.farhan, name='farhan'),
+    path('drinks/', views.drinks, name='drinks'),
     path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
 ]
