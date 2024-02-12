7b1260f2e7446a43703da355e98e7a922f2eba88
Farhan_waseer <59307895+farhanwaseer@users.noreply.github.com>
2024-02-12 16:47:29 +0000

    aaa

--- DjangoWork/project-one/myproject/myapp/urls.py
+++ DjangoWork/project-one/myproject/myapp/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('farhan/', views.farhan, name='farhan'),
-    path('drinks/', views.drinks, name='drinks'),
-    path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name='menu'),
+    path('book/', views.book, name='book'),
+    # path('drinks/', views.drinks, name='drinks'),
+    path('drinks/<str:drink_name>',views.drinks,name='drink_name'),    
 ]
--- DjangoWork/project-one/myproject/myapp/views.py
+++ DjangoWork/project-one/myproject/myapp/views.py
@@ -11,12 +11,32 @@ def home(request):
 def farhan(request):
     return HttpResponse("<h1>Hello world! Django app created BY FARHAN WASEER")
 
+# def drinks(request,drink_name):
+#     drink = {
+#         'mocha' : 'type of coffee',
+#         'tea' : 'type of hot beverage',
+#         'lemonade': 'type of refreshment'
+#     }
+#     choice_of_drink = drink[drink_name]
+#     return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
+
+
+
 def drinks(request, drink_name):
     drink = {
-        'mocha' : 'type of coffee',
+       'mocha' : 'type of coffee',
         'tea' : 'type of hot beverage',
-        'lemonade': 'type of refreshment'
+        'lemonade': 'type of refreshment',
     }
-    choice_of_drink = drink[drink_name]
-    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
 
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f'<h2>{drink_name}</h2>' + choice_of_drink)
+
+def about(response):
+    return HttpResponse('<h2>About</h2>')
+
+def menu(response):
+    return HttpResponse('<h2>Menu</h2>')
+
+def book(response):
+    return HttpResponse('<h2>Book</h2>')
